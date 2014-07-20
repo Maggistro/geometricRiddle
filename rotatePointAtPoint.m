70af8585483fc7dc7c312b9524392d98686bbcaa
Maximilian Mühlfeld <maggistro@gmx.net>
2014-07-21 01:31:26 +0200

    fix for rotatePointAtPoint

--- rotatePointAtPoint.m
+++ rotatePointAtPoint.m
@@ -1,7 +1,7 @@
 function [newPoint] = rotatePointAtPoint(oldPoint,mid,angle)
 %rotates one point around another
         newPoint(1) = (oldPoint(1)-mid(1))*cos(angle) -...
-             (oldPoint(2)-mid(2))*sin(angleDiff) + mid(1);
+             (oldPoint(2)-mid(2))*sin(angle) + mid(1);
          newPoint(2) = (oldPoint(1)-mid(1))*sin(angle) +...
-             (oldPoint(2)-mid(2))*cos(angleDiff) + mid(2);
+             (oldPoint(2)-mid(2))*cos(angle) + mid(2);
 end
